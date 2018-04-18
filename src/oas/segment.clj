0c9ebff116e8bb184cef944e68049a12ffb5018b
Scott Olsen <scg.olsen@gmail.com>
2018-04-18 11:21:55 -0400

    Refactor modules.

---
+++ src/oas/segment.clj
@@ -0,0 +1,46 @@
+(ns oas.segment
+  "Access segments of an OAS document.")
+
+(defn segment [oas k] 
+  "Get a segment of a parsed OAS document."
+  (let [kw (keyword k)] 
+    {kw (get oas kw nil)}))
+
+(defn segments [oas parts]
+  "Return the specified parts of the OAS document."
+  (map #(segment oas %) (set parts)))
+
+(defn specification [oas] 
+  "Identify the version of an OAS file."
+  (let [parsed (parse-string oas true)]
+    (if (contains? parsed :swagger) 
+        {:swagger (get parsed :swagger)} 
+        {:openapi (get parsed :openapi)})))
+
+(defn paths [oas] 
+  "Get the paths object of an OAS document."
+  (segment oas :paths))
+
+(defn info [oas] 
+  "Get the info object of an OAS document."
+  (segment oas :info))
+
+(defn servers [oas]
+  "Get the servers segment of an OAS document."
+  (segment oas :servers))
+
+(defn components [oas] 
+  "Get the components segment of an OAS document."
+  (segment oas :components))
+
+(defn security [oas] 
+  "Get the security segment of an OAS document."
+  (segment oas :security))
+
+(defn tags [oas]
+  "Get the tags segment of an OAS document."
+  (segment oas :tags))
+
+(defn externalDocs [oas] 
+  "Get the externalDocs segment of an OAS document."
+  (segment oas :externalDocs))
