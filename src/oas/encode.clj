f6667cbdfd042f852287e4543b0af15a729adcca
Scott Olsen <scg.olsen@gmail.com>
2018-04-19 17:46:11 -0400

    Update encode.

--- src/oas/encode.clj
+++ src/oas/encode.clj
@@ -1,41 +1,10 @@
 (ns oas.encode
   "Encode an OAS document."
-  (:require [cheshire.core :refer :all]))
+  (:require [cheshire.core :as che] [clojure.java.io :as io] 
+            [oas.validate :as validate]))
 
-(defn oas [part] 
-  "Encode an oas document."
-  {part})
-
-(defn info [part] 
-  "Encode an info object."
-  {:info part})
-
-(defn definitions [part] 
-  "Encode a definitions object."
-  {:definitions part})
-
-(defn servers [part] 
-  "Encode a servers object."
-  {:servers part})
-
-(defn paths [part] 
-  "Encode a paths object."
-  {:paths part})
-
-(defn components [part] 
-  "Encode a components object."
-  {:components part})
-
-(defn security [part] 
-  "Encode a security object."
-  {:security part})
-
-(defn tags [part] 
-  "Encode a tags object."
-  {:tags part})
-
-(defn externalDocs [part] 
-  "Encode an externalDocs object."
-  {:externalDocs part})
+(defn encode [part] 
+  "Encode an OAS document or part."
+  (validate/conform part))
 
 
