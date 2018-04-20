d2d3e3d4c061d990f8de7bfb5438fe6b233ab71a
Scott Olsen <scg.olsen@gmail.com>
2018-04-20 11:14:42 -0400

    Refactor removal functions. Add remove-xs fn generator.

--- src/oas/alter.clj
+++ src/oas/alter.clj
@@ -3,52 +3,51 @@
   (:require [oas.resolve :as res]))
 
 (defn remove-by  
-  "Returns a function that removes the contents of a segment based on a predicate.
-   The function first runs the provided f over the part, and the g over the predicate."
+  "Returns a function that removes the contents of a map based on a predicate.
+   f is applied to the part, 
+   g is applied to the predicate--a predicate modifier."
   ([f] 
    (fn [p pred] 
-       (assoc {} (key (first p)) 
-                 (into {} (remove #(pred (f %)) (val (first p)))))))
+       (into {} (remove #(pred (f %)) p))))
   ([f g] 
    (fn [p pred] 
-       (assoc {} (key (first p)) 
-                 (into {} (remove #(g (pred (f %))) (val (first p))))))))
+       (into {} (remove #(g (pred (f %))) p)))))
 
 (def remove-by-key (remove-by (comp name first)))
-
 (def remove-by-value (remove-by second))
-
 (def filter-by-key (remove-by (comp name first) not))
-
 (def filter-by-value (remove-by second not))
 
-(defn remove-keys [part ks] 
-  "Apply multiple filters to a part."
-  (loop [k ks result part]
-        (if (empty? k) 
-          result 
-          (recur (rest k) (remove-by-key part #(= (first k) %))))))
+(defn remove-xs [removal pred]
+  "Takes a remove function and a list of things to remove. 
+   Removes each x in xs that match the predicate.
+   Provides a defualt predicate of = for simple removal by matching.
+   You can also use other predicates, such as re-matches. e.g.
+   (remove-keys part [#\".*Pet\"] re-matches"
+  (fn
+    ([part xs]
+      (loop [x xs result part] 
+            (if (empty? x) 
+                result
+                (recur (rest x) (removal result #(pred (first x) %)))))) 
+    ([part xs predicate] 
+      (loop [x xs result part] 
+            (if (empty? x) 
+                result
+                (recur (rest x) (removal result #(predicate (first x) %))))))))
 
-(defn filter-keys [part ks] 
-  "Apply multiple filters to a part."
-  (loop [k ks result part]
-        (if (empty? k) 
-          result 
-          (recur (rest k) (filter-by-key part #(= (first k) %))))))
-
-(defn filter-values [part vs]
-  "Apply multiple value filters to a part."
-  (loop [v vs result part]
-        (if (empty? v) 
-          result 
-          (recur (rest v) (filter-by-value part #(= (first v) %))))))
+(def remove-keys (remove-xs remove-by-key =))
+(def remove-values (remove-xs remove-by-value =))
+(def filter-keys (remove-xs filter-by-key =))
+(def filter-values (remove-xs filter-by-value =))
 
 (defn append-to [target k v] 
-  "Append a key/val to a target part. Does not modify contents if the key already exists."
+  "Append a key/val to a target part. 
+   Does not modify contents if the key already exists."
   (if (contains? target k) 
       target 
       (assoc target k v)))
 
 (defn modify [target k v]
-  "Update a key with value."
+  "Update a key with a value."
   (assoc target k v))
