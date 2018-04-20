c9395764ad6b3b73b3b5a5e0d7cd2ee0ae3abf51
Scott Olsen <scg.olsen@gmail.com>
2018-04-20 11:13:17 -0400

    Add parse-file.

--- src/oas/parse.clj
+++ src/oas/parse.clj
@@ -1,7 +1,11 @@
 (ns oas.parse 
   "Parse OAS files."
-  (:require [cheshire.core :refer :all]))
+  (:require [cheshire.core :as che]))
 
 (defn parse [oas] 
-  "Parse the entirety of an OAS file." 
-  (parse-string oas true))
+  "Parse the entirety of an OAS string." 
+  (che/parse-string oas true))
+
+(defn parse-file [file]
+  "Parse the entirety of an OAS file."
+  (che/parse-string (slurp file) true))
