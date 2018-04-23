ffdca734b1d1447ad9cabd91cf0b927d67a39b25
Scott Olsen <scg.olsen@gmail.com>
2018-04-23 17:24:15 -0400

    Add reducing functions.

---
+++ src/oas/reduce.clj
@@ -0,0 +1,12 @@
+(ns oas.reduce
+  "Reducing functions.")
+
+(defn append-to 
+  [api kvs] 
+
+defn append-when 
+
+(defn modify [target k v]
+  "Update a key with a value."
+  (assoc target k v))
+
