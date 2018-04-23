bdc075562c2d9230243317a641a61f000a4ec91a
Scott Olsen <scg.olsen@gmail.com>
2018-04-23 17:24:47 -0400

    Update validation for new spec strategy.

--- src/oas/validate.clj
+++ src/oas/validate.clj
@@ -1,39 +1,15 @@
 (ns oas.validate
   "Validate the contents of an OAS document or part."
-  (:require [clojure.set :as S][oas.specs.swagger :as swagger]
+  (:require [clojure.set :as S][oas.specs.swagger.root]
             [oas.specs.oas :as oas][clojure.spec.alpha :as s]))
 
-;;; specification {:info true :servers false ...}
-;;; A map of key names and booleans. A boolean indicates whether the key is
-;;; required.
+(defn- examine-spec [f]
+  (fn 
+    ([api spec] 
+     (f (keyword (str "oas.specs." spec ".root") "root") api))
+    ([api spec object] 
+     (f (keyword (str "oas.specs." spec ".root") object) api))))
 
-(defn requirements [spec] 
-  "Get the required fields of a spec."
-  (keys (filter #(= true (val %)) spec)))
-
-(defn missing-reqs [part spec]
-  "Return the set of missing required keys."
-  (S/difference (set (requirements spec)) (set (keys part))))
-
-(defn unknowns [part spec]
-  "Return the set of unrecognized keys."
-  (S/difference (set (keys part)) (set (keys spec))))
-
-(defn requirements-met? [part spec] 
-  "Returns a boolean indicating whether all required fields are present."
-  (S/subset? (set (keys (requirements spec))) (set (keys part))))
-
-(defn valid? [part spec t]
-  "Validate an OAS document part. 
-   A part is valid iff the set of missing and set of unknowns are empty."
-  (s/valid? (keyword (str "oas.specs." spec) t) part))
-
-(defn conform [part spec t]
-  "Validate an OAS document part. 
-   A part is valid iff the set of missing and set of unknowns are empty."
-  (s/conform (keyword (str "oas.specs." spec) t) part))
-
-(defn explain [part spec t]
-  "Validate an OAS document part. 
-   A part is valid iff the set of missing and set of unknowns are empty."
-  (s/explain (keyword (str "oas.specs." spec) t) part))
+(def valid? (examine-spec s/valid?))
+(def conform (examine-spec s/conform))
+(def explain (examine-spec s/explain))
