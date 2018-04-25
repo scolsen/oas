9e54371532ea26219b8d921914d83ca176ccb3d0
Scott Olsen <scg.olsen@gmail.com>
2018-04-25 13:25:24 -0400

    Add constraint support.

--- src/oas/validate.clj
+++ src/oas/validate.clj
@@ -10,6 +10,10 @@
     ([api spec object] 
      (f (keyword (str "oas.specs." spec ".root") object) api))))
 
+(defmacro constrain [spec object predicate] 
+  (let [k (keyword (str "oas.specs." spec) object)]
+       `(s/def ~k (s/and (s/get-spec ~k) ~predicate))))
+
 (def valid? (examine-spec s/valid?))
 (def conform (examine-spec s/conform))
 (def explain (examine-spec s/explain))
