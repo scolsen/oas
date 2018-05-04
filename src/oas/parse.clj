8d58bd129fd92b1a772f0eef3cec3435af73ed1d
Scott Olsen <scg.olsen@gmail.com>
2018-05-04 01:05:21 -0400

    Fix conversion of / in json keys.

--- src/oas/parse.clj
+++ src/oas/parse.clj
@@ -1,6 +1,11 @@
 (ns oas.parse 
   "Parse OAS files."
-  (:require [cheshire.core :as che]))
+  (:require [cheshire.core :as che]
+            [clojure.string :as s]))
+
+(defn- kstring->key 
+  "Coerce a swagger key, which may contain /, to a usable clojure key."
+  [k] (keyword (s/replace k #"/" "_")))
 
 (defn parse [oas] 
   "Parse the entirety of an OAS string." 
@@ -8,4 +13,4 @@
 
 (defn parse-file [file]
   "Parse the entirety of an OAS file."
-  (che/parse-string (slurp file) true))
+  (che/parse-string (slurp file) kstring->key))
