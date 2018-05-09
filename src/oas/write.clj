e17f12f6674b43f22a280f38ad0fe5137588fd90
Scott Olsen <scg.olsen@gmail.com>
2018-05-09 11:19:35 -0400

    Comment out write functions temporarily

--- src/oas/write.clj
+++ src/oas/write.clj
@@ -9,29 +9,29 @@
   ([encoded] (che/generate-string encoded))
   ([encoded file] (spit file (che/generate-string encoded))))
 
-(defn append-file 
-  "Append a key and value to a segment and write to a file.
-   Reads the in file, and parses the json.
-   appends the key and value to the in file.
-   writes the result out to the out file."
-  ([in out k v]
-   (-> in 
-       (p/parse-file)
-       (u/append-to k v)
-       (write out))) 
-  ([in out k v reference] 
-   (let [parsed (p/parse-file in)]
-        (-> reference 
-            (->> (u/modify parsed k v))
-            (write out)))))
-
-;; add compatibility checking. i.e. 2.0 cannot be merged to 3.0.
-;; later add support for merging 2.0 and 3.0 
-(defn merge-files
-  "Merge the contents of one oas file with another."
-  [in in* out]
-  (let [oas (p/parse-file in) oas* (p/parse-file in*)]
-       (-> oas
-           (u/merge-oas oas*)
-           (write out))))
-
+;;(defn append-file 
+;;  "Append a key and value to a segment and write to a file.
+;;   Reads the in file, and parses the json.
+;;   appends the key and value to the in file.
+;;   writes the result out to the out file."
+;;  ([in out k v]
+;;   (-> in 
+;;       (p/parse-file)
+;;       (u/append-to k v)
+;;       (write out))) 
+;;  ([in out k v reference] 
+;;   (let [parsed (p/parse-file in)]
+;;        (-> reference 
+;;            (->> (u/update-value parsed k v))
+;;            (write out)))))
+;;
+;;;; add compatibility checking. i.e. 2.0 cannot be merged to 3.0.
+;;;; later add support for merging 2.0 and 3.0 
+;;(defn merge-files
+;;  "Merge the contents of one oas file with another."
+;;  [in in* out]
+;;  (let [oas (p/parse-file in) oas* (p/parse-file in*)]
+;;       (-> oas
+;;           (u/merge-json oas*)
+;;           (write out))))
+;;
